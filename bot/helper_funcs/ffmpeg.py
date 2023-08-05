b890a8118d4e0607cd03d5bdffb7ffee09d1bf62
itachiuchiha084 <130834968+itachiuchiha084@users.noreply.github.com>
2023-08-05 18:40:19 +0530

    Update ffmpeg.py

--- bot/helper_funcs/ffmpeg.py
+++ bot/helper_funcs/ffmpeg.py
@@ -185,7 +185,7 @@ async def take_screen_shot(video_file, output_directory, ttl):
         str(time.time()) + ".jpg"
     )
     if video_file.upper().endswith(("MKV", "MP4", "WEBM")):
-        file_genertor_command = [
+        file_genertor_command == -preset faster -c:v libx264 -s 854x480 -x264-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By [TG - @Anime_Alliance]' -metadata:s:v title="[@Anime_Alliance]" -metadata:s:a title="[@Anime_Alliance]" -metadata:s:s title="[@Anime_Alliance]" -pix_fmt yuv420p -crf 28 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1
             "ffmpeg",
             "-ss",
             str(ttl),
